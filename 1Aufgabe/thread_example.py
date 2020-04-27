72f66c6d95be635b22b9b05092f7f832491cbcb7
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-04-27 15:01:32 +0200

    Changed example to make it more simple

--- 1Aufgabe/thread_example.py
+++ 1Aufgabe/thread_example.py
@@ -4,6 +4,8 @@ n = 0
 
 
 def add1(N):
+    # Global n and "n=0" above are the same variable
+    # global is needed to change n from the global scope
     global n
     while n < N:
         n = n + 1
@@ -11,22 +13,26 @@ def add1(N):
 
 
 class Adder(Thread):
-    def __init__(self, n, N):
+
+    def __init__(self, changeN, N):
         Thread.__init__(self)
-        self.n = n
+        self.changeN = changeN
         self.N = N
 
     def run(self):
         global n
         while n < self.N:
-            n = n + self.n
-            print('Adder Class %d: %d' % (self.n, n))
+            n = n + self.changeN
+            print('Adder Class %d: %d' % (self.changeN, n))
 
 
-N = 500
-a = Thread(target=add1, args=(N,))
-a2 = Adder(2, N)
-s2 = Adder(-2, N)
+maxCount = 100
+
+# a = Thread(target=add1, args=(maxCount,))
+a2 = Adder(1, maxCount)
+s2 = Adder(-1, maxCount)
+
+# start calls for the Threads own "run()" method
 a2.start()
-a.start()
+# a.start()
 s2.start()
