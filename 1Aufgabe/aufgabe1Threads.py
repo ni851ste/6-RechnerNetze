1645fb11996c7a37fb1390e2598a1a6b5974a81b
Nik Steinbruegge <ni851ste@htwg-konstanz.de>
2020-05-03 13:42:06 +0200

    Customers can buy more than 1 product

--- 1Aufgabe/aufgabe1Threads.py
+++ 1Aufgabe/aufgabe1Threads.py
@@ -13,13 +13,23 @@ from time import sleep
 # Kasse                 30s             20                   3
 # Baecker               20s             20                   3
 #                        145
+
+
 # Baecker   per article     10s
 # Wurst     per article     30s
 # Kaese     per article     60s
 # Kasse     per article     5s
 
 
+typ1Specs = [(10, 10, 10),  # Baecker
+             (30, 10, 5),  # Wurst
+             (45, 5, 3),  # Kaese
+             (60, 20, 30)]  # Kasse
 
+typ2Specs = [(20, 20, 3),  # Baecker
+             (30, 5, 2),  # Wurst
+             (0, 0, 0),  # Kaese
+             (30, 20, 3)]  # Kasse
 
 
 class Kunde(Thread):
@@ -29,28 +39,19 @@ class Kunde(Thread):
         self.name = name
         self.waitToBeServedEv = Event()
         if id == 1:
-            self.nochBesuchendeStationen = [(10, 10, 10),  # Baecker
-                                            (30, 10, 5),  # Wurst
-                                            (45, 5, 3),  # Kaese
-                                            (60, 20, 30)]  # Kasse
+            self.nochBesuchendeStationen = typ1Specs
             self.nochBesuchen = [0, 1, 2, 3]
         else:
-            self.nochBesuchendeStationen = [(20, 20, 3),  # Baecker
-                                            (30, 5, 2),  # Wurst
-                                            (0, 0, 0),  # Kaese
-                                            (30, 20, 3)]  # Kasse
+            self.nochBesuchendeStationen = typ2Specs
             self.nochBesuchen = [1, 3, 0]
 
         self.nextStation = 0
-
-
 
     def goToStation(self):
         self.nextStation = self.nochBesuchen.pop(0)
         print("customer: " + self.name + " goes to Station " + stations[self.nextStation].name + ".")
 
         ownSleep(self.nochBesuchendeStationen[self.nextStation][0])
-
 
     def arriveAtStation(self):
         currentStation = stations[self.nextStation]
@@ -64,20 +65,22 @@ class Kunde(Thread):
 
         self.startStation()
 
-
     def startStation(self):
-        print("customer: " + self.name + " started at Station " + stations[self.nextStation].name + ".")
-
         currentStation = stations[self.nextStation]
+        print("customer: " + self.name + " started at Station " + currentStation.name + ".")
+
+        currentStation.serveTimeForNextCustomer = currentStation.timePerProduct * \
+                                                  self.nochBesuchendeStationen[self.nextStation][2]
+
+        # TODO setting when coming from Q may have a bad outcome
         currentStation.ownArrEv.set()
 
-        print("customer: " + self.name + " is being served " + stations[self.nextStation].name + ".")
+        print("customer: " + self.name + " is being served " + currentStation.name + ".")
         currentStation.ownServEv.wait()
 
-        print("customer: " + self.name + " finished at Station " + stations[self.nextStation].name + ".")
+        print("customer: " + self.name + " finished at Station " + currentStation.name + ".")
 
-
-    #def finishedAtStation(self, argList):
+    # def finishedAtStation(self, argList):
 
     def run(self):
         print("customer: " + self.name + " started.")
@@ -89,19 +92,19 @@ class Kunde(Thread):
         print("customer: " + self.name + " finished shopping")
 
 
-
-
 class Station(Thread):
     # TODO Make stations every its own instance
-    def __init__(self, name, abarbeitungsDauer, ownArrEv, ownServEv):
+    def __init__(self, name, timePerProduct):
         print(name + " started to init.")
         Thread.__init__(self)
         self.name = name
-        self.abarbeitungsDauer = abarbeitungsDauer
+        self.timePerProduct = timePerProduct
+        self.serveTimeForNextCustomer = 0
+
         self.warteSchlange = []
         self.bedientGerade = False
-        self.ownArrEv = ownArrEv
-        self.ownServEv = ownServEv
+        self.ownArrEv = Event()
+        self.ownServEv = Event()
         self.nextInQueue = Event()
 
     def waitForCustomer(self):
@@ -113,19 +116,15 @@ class Station(Thread):
         print(self.name + " exited by Event.")
         exit(11)
 
-
     def serve(self):
         # TODO how much articles are bought
         print("station: " + self.name + " started to serve.")
-        ownSleep(self.abarbeitungsDauer)
+        ownSleep(self.serveTimeForNextCustomer)
         print("station: " + self.name + " finished to serve.")
 
         self.ownServEv.set()
 
-
         self.ownServEv.clear()
-
-
 
     def run(self):
         print(self.name + " started")
@@ -144,9 +143,8 @@ class Station(Thread):
             self.ownArrEv.clear()
 
 
-
-SLEEP_INTERVAL = 0.05
-SIMULATION_LENGTH = 350
+SLEEP_INTERVAL = 0.005
+SIMULATION_LENGTH = 1000
 globalTimeCounter = 0
 
 
@@ -162,22 +160,12 @@ def startCustomer(name, typ):
     kunde.start()
 
 
-baeckArrEv = Event()
-wurstArrEv = Event()
-kaeseArrEv = Event()
-kasseArrEv = Event()
-
-baeckServEv = Event()
-wurstServEv = Event()
-kaeseServEv = Event()
-kasseServEv = Event()
-
 globalStationStopEvent = Event()
 
-stations = [Station("Baecker", 10, baeckArrEv, baeckServEv),
-            Station("Wurst", 30, wurstArrEv, wurstServEv),
-            Station("Kaese", 60, kaeseArrEv, kaeseServEv),
-            Station("Kasse", 5, kasseArrEv, kasseServEv)]
+stations = [Station("Baecker", 10),
+            Station("Wurst", 30),
+            Station("Kaese", 60),
+            Station("Kasse", 5)]
 
 for stat in stations:
     stat.start()
@@ -189,12 +177,11 @@ print("main: started to sleep")
 for i in range(0, SIMULATION_LENGTH):
     print("CurrentTime: " + str(globalTimeCounter))
 
-    if globalTimeCounter == 5:
-        startCustomer("2-Typ1", 1)
+    # if globalTimeCounter == 5:
+    #    startCustomer("2-Typ1", 1)
 
     sleep(SLEEP_INTERVAL)
     globalTimeCounter = globalTimeCounter + 1
-
 
 print("main: stopping all stations")
 globalStationStopEvent.set()
@@ -202,5 +189,3 @@ globalStationStopEvent.set()
 sleep(2)
 print("main: killing main")
 exit(11)
-
-
